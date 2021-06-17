8fef89069cfc8e40ac99a7b263c68745a361033e
Sumit Mishra <sumit@macbooks-MacBook-Pro-3.local>
2021-06-18 00:13:37 +0530

    Added Scan History layout and class

---
+++ app/src/main/java/com/example/qrscanner/activity/mainActivity/view/ScanHistoryFragment.kt
@@ -0,0 +1,12 @@
+package com.example.qrscanner.activity.mainActivity.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.qrscanner.R
+
+class ScanHistoryFragment : Fragment(R.layout.fragment_scan_history) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
